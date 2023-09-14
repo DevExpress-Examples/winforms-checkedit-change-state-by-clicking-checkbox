e9d3500ddf172c25423dc5a2ad7ee670f0df375f
DevExpressExampleBot <fakeEmail>
2023-09-14 22:51:29 +0400

    Source auto update [skip ci]

--- VB/Q181266/Form1.Designer.vb
+++ VB/Q181266/Form1.Designer.vb
@@ -1,59 +1,56 @@
-Imports Microsoft.VisualBasic
-Imports System
 Namespace Q181266
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.checkEdit1 = New DevExpress.XtraEditors.CheckEdit()
-			CType(Me.checkEdit1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' checkEdit1
-			' 
-			Me.checkEdit1.Location = New System.Drawing.Point(38, 26)
-			Me.checkEdit1.Name = "checkEdit1"
-			Me.checkEdit1.Properties.Caption = "checkEdit1"
-			Me.checkEdit1.Size = New System.Drawing.Size(81, 19)
-			Me.checkEdit1.TabIndex = 0
-'			Me.checkEdit1.MouseDown += New System.Windows.Forms.MouseEventHandler(Me.checkEdit1_MouseDown);
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(872, 597)
-			Me.Controls.Add(Me.checkEdit1)
-			Me.Name = "Form1"
-			Me.Text = "Form1"
-			CType(Me.checkEdit1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.checkEdit1 = New DevExpress.XtraEditors.CheckEdit()
+            CType((Me.checkEdit1.Properties), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' checkEdit1
+            ' 
+            Me.checkEdit1.Location = New System.Drawing.Point(38, 26)
+            Me.checkEdit1.Name = "checkEdit1"
+            Me.checkEdit1.Properties.Caption = "checkEdit1"
+            Me.checkEdit1.Size = New System.Drawing.Size(81, 19)
+            Me.checkEdit1.TabIndex = 0
+            AddHandler Me.checkEdit1.MouseDown, New System.Windows.Forms.MouseEventHandler(AddressOf Me.checkEdit1_MouseDown)
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(872, 597)
+            Me.Controls.Add(Me.checkEdit1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            CType((Me.checkEdit1.Properties), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+        End Sub
 
-		Private WithEvents checkEdit1 As DevExpress.XtraEditors.CheckEdit
-	End Class
+#End Region
+        Private checkEdit1 As DevExpress.XtraEditors.CheckEdit
+    End Class
 End Namespace
-
--- VB/Q181266/Form1.vb
+++ VB/Q181266/Form1.vb
@@ -1,30 +1,25 @@
-Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
-Imports System.Data
 Imports System.Drawing
-Imports System.Text
 Imports System.Windows.Forms
 Imports DevExpress.XtraEditors
 Imports DevExpress.XtraEditors.ViewInfo
 Imports DevExpress.Utils
 
 Namespace Q181266
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-		End Sub
 
-		Private Sub checkEdit1_MouseDown(ByVal sender As Object, ByVal e As MouseEventArgs) Handles checkEdit1.MouseDown
-			Dim cEdit As CheckEdit = CType(sender, CheckEdit)
-			Dim cInfo As CheckEditViewInfo = CType(cEdit.GetViewInfo(), CheckEditViewInfo)
-			Dim r As Rectangle = cInfo.CheckInfo.GlyphRect
-			Dim editorRect As New Rectangle(New Point(0, 0), cEdit.Size)
-			If (Not r.Contains(e.Location)) AndAlso editorRect.Contains(e.Location) Then
-				CType(e, DXMouseEventArgs).Handled = True
-			End If
-		End Sub
-	End Class
+    Public Partial Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub checkEdit1_MouseDown(ByVal sender As Object, ByVal e As MouseEventArgs)
+            Dim cEdit As CheckEdit = CType(sender, CheckEdit)
+            Dim cInfo As CheckEditViewInfo = CType(cEdit.GetViewInfo(), CheckEditViewInfo)
+            Dim r As Rectangle = cInfo.CheckInfo.GlyphRect
+            Dim editorRect As Rectangle = New Rectangle(New Point(0, 0), cEdit.Size)
+            If Not r.Contains(e.Location) AndAlso editorRect.Contains(e.Location) Then CType(e, DXMouseEventArgs).Handled = True
+        End Sub
+    End Class
 End Namespace
